a4693810745ff000c7edee196cc0b6676c7e97e2
Sam Mariscal <sammiermariscal@gmail.com>
2025-11-25 18:51:29 +0800

    few tweaks lang gud , pagud

--- Front_End/Nav_bar.php
+++ Front_End/Nav_bar.php
@@ -49,19 +49,7 @@ if (isset($_GET['action']) && $_GET['action'] === 'logout') {
                 d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
         </svg>
       <!--Hi, text balhin ngari -->
-      <?php
-      $firstName = 'Guest';
-      $lastName  = ''; 
-
-
-      if (isset($_SESSION['first_name'])) {
-      $firstName = $_SESSION['first_name'];
-      }
-
-      echo "Hello, " . htmlspecialchars($firstName);
-      ?>
-
-      </a>
+      
       <!--message icon-->
         <a href="https://samtu43.github.io/Sam_chanel/" class="flex items-center hover:text-amber-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
@@ -122,6 +110,16 @@ $isLoggedIn = isset($_SESSION['user_id']); // Replace 'user_id' with your sessio
     <div id="profileDropdown" class="hidden absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg z-10">
         <?php if ($isLoggedIn): ?>
             <!-- Logged-in user dropdown -->
+             <div>
+              <?php if (isset($_SESSION['first_name']) && !empty($_SESSION['first_name'])): ?>
+  <div class="hidden sm:flex sm:items-center sm:ml-4">
+    <div style="line-height:1;">
+      <div class="text-sm italic font-medium text-gray-800">Welcome  <span class="text-sm font-bold italic" style="color:#F59E0B; text-transform:uppercase;"><?= htmlspecialchars($_SESSION['first_name']); ?></span></div>
+      
+    </div>
+  </div>
+<?php endif; ?>
+             </div>
             <a href="#" class="block px-4 py-2 hover:bg-gray-100">Profile</a>
             <a href="#" class="block px-4 py-2 hover:bg-gray-100">Reviews</a>
             <a href="#" class="block px-4 py-2 hover:bg-gray-100">Wishlist</a>
