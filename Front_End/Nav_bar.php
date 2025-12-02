2b59f05e417f7778296266e1e0208bad068f121c
James Arnoco <jamesarnoco2006@gmail.com>
2025-12-02 17:10:43 +0800

    Sticky pages

--- Front_End/Nav_bar.php
+++ Front_End/Nav_bar.php
@@ -27,7 +27,7 @@ if (isset($_GET['action']) && $_GET['action'] === 'logout') {
         font-weight: 400;
     }
 </style>
-<nav class="bg-white  ">
+<nav class="bg-white shadow-sm sticky top-0 w-full z-20">
     <div class="max-w-7xl mx-auto px-4 py-2 flex flex-wrap items-center justify-between gap-2">
 
         <a href="index.php" class="flex items-center flex-shrink-0">
@@ -118,14 +118,32 @@ $isLoggedIn = isset($_SESSION['user_id']);
     <div id="profileDropdown" class="hidden absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg z-10">
         <?php if ($isLoggedIn): ?>
 
-            <?php if (!empty($_SESSION['first_name'])): ?>
-                <div class="px-4 py-2 chewy-font-v2">
-                    Welcome 
-                    <span class="font-bold italic uppercase text-yellow-500">
-                        <?= htmlspecialchars($_SESSION['first_name']) . '!'; ?>
-                    </span>
-                </div>
-            <?php endif; ?>
+    <?php if (!empty($_SESSION['first_name'])): ?>
+    <?php 
+    $greetings = [
+    "Welcome back,",
+    "Hello there,",
+    "Hi!,",
+    "Hey!,",
+    "Good to see you,",
+    "Let’s shop,",
+    "Hot deals await,",
+    "Your wishlist awaits,",
+    "Check your orders,",
+    "New arrivals!,"
+
+    ];
+
+    $greeting = $greetings[array_rand($greetings)];
+    $firstName = htmlspecialchars($_SESSION['first_name'])
+    ?>
+        <div class="px-4 py-2">
+          <?= $greeting ?>
+            <span class="font-bold text-black-200">
+                <?= $firstName  . '!'; ?>
+            </span>
+        </div>
+    <?php endif; ?>
 
             <a href="profile.php" class="flex items-center px-4 py-2 hover:bg-gray-100 gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
@@ -159,7 +177,7 @@ $isLoggedIn = isset($_SESSION['user_id']);
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
                 </svg>
-                <span>become a seller </span>
+                <span>Become a seller </span>
             </a>
 
             <a href="#" class="flex items-center px-4 py-2 hover:bg-gray-100 gap-2">
